23f77ce7ab7ee9ab89790eec7501d281a6dc8ff6
setlol <dovnar.maxim@gmail.com>
2016-11-16 11:35:37 +0300

    Add files via upload

---
+++ class/DoubleRandomGeneration.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "DoubleRandomGeneration.h"
+
+
+DoubleRandomGeneration::DoubleRandomGeneration()
+{
+}
+
+
+DoubleRandomGeneration::~DoubleRandomGeneration()
+{
+}
+_____
+|lol|
+|___|
